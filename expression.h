bfc24637b7e122a96320c9c75913acb585aae0f4
Yc7521 <752106133@qq.com>
2020-05-11 20:57:24 +0800

    update

--- expression.h
+++ expression.h
@@ -1,25 +1,41 @@
 #ifndef AUTOMATIC_DERIVATION_EXPRESSION_H
 #define AUTOMATIC_DERIVATION_EXPRESSION_H
 
+#include <string>
+#include <string_view>
+#include <typeinfo>
+
 namespace expression {
   class Expr {
     public:
       virtual ~Expr() = default;
+      virtual std::string to_string() const { return ""; }
   };
 
   template<class T>
   class Constant : public Expr {
     public:
-      explicit Constant(T value) : _value(value) {
-      }
+      explicit Constant(T value) : _value(value) {}
       ~Constant() override = default;
 
-      decltype(auto) expr() const {
-          return _value;
-      }
+      decltype(auto) expr() const { return _value; }
+      std::string to_string() const override { return ::to_string(_value); }
 
     private:
       const T _value;
+  };
+
+  template<class T>
+  class Variable : public Expr {
+    public:
+      explicit Variable(std::string_view name) : _name(name) {}
+      ~Variable() override = default;
+
+      decltype(auto) name() const { return _name; }
+      std::string to_string() const override { return _name; }
+
+    private:
+      std::string _name;
   };
 
   template<size_t args>
@@ -32,12 +48,13 @@ namespace expression {
   template<>
   class Operator<1> : public Expr {
     public:
-      explicit Operator(Expr *expr) : _expr(expr) {
-      }
+      explicit Operator(Expr *expr) : _expr(expr) {}
       ~Operator() override = default;
 
-      decltype(auto) expr() {
-          return _expr;
+      decltype(auto) expr() { return _expr; }
+      decltype(auto) expr() const { return _expr; }
+      std::string to_string() const override {
+          return "?(" + expr()->to_string() + ")";
       }
 
     private:
@@ -47,15 +64,15 @@ namespace expression {
   template<>
   class Operator<2> : public Expr {
     public:
-      Operator(Expr *expr1, Expr *expr2) : _expr1(expr1), _expr2(expr2) {
-      }
+      Operator(Expr *expr1, Expr *expr2) : _expr1(expr1), _expr2(expr2) {}
       ~Operator() override = default;
 
-      decltype(auto) expr_left() {
-          return _expr1;
-      }
-      decltype(auto) expr_right() {
-          return _expr2;
+      decltype(auto) expr_left() { return _expr1; }
+      decltype(auto) expr_right() { return _expr2; }
+      decltype(auto) expr_left() const { return _expr1; }
+      decltype(auto) expr_right() const { return _expr2; }
+      std::string to_string() const override {
+          return expr_left()->to_string() + " ? " + expr_right()->to_string();
       }
 
     private:
@@ -65,55 +82,72 @@ namespace expression {
 
   class Negative : public Operator<1> {
     public:
-      explicit Negative(Expr *expr) : Operator(expr) {
-      }
+      explicit Negative(Expr *expr) : Operator(expr) {}
       ~Negative() override = default;
+      std::string to_string() const override { return "-" + expr()->to_string(); }
   };
 
   class Sine : public Operator<1> {
-     public:
+    public:
       explicit Sine(Expr *expr) : Operator(expr) {}
       ~Sine() override = default;
+      std::string to_string() const override {
+          return "sin(" + expr()->to_string() + ")";
+      }
   };
 
   class Cosine : public Operator<1> {
-     public:
+    public:
       explicit Cosine(Expr *expr) : Operator(expr) {}
       ~Cosine() override = default;
+      std::string to_string() const override {
+          return "cos(" + expr()->to_string() + ")";
+      }
   };
 
   class Tangent : public Operator<1> {
-     public:
+    public:
       explicit Tangent(Expr *expr) : Operator(expr) {}
       ~Tangent() override = default;
+      std::string to_string() const override {
+          return "tan(" + expr()->to_string() + ")";
+      }
   };
 
   class Add : public Operator<2> {
     public:
-      Add(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {
-      }
+      Add(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {}
       ~Add() override = default;
+      std::string to_string() const override {
+          return expr_left()->to_string() + " + " + expr_right()->to_string();
+      }
   };
 
   class Minus : public Operator<2> {
     public:
-      Minus(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {
-      }
+      Minus(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {}
       ~Minus() override = default;
+      std::string to_string() const override {
+          return expr_left()->to_string() + " - " + expr_right()->to_string();
+      }
   };
 
   class Multiply : public Operator<2> {
     public:
-      Multiply(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {
-      }
+      Multiply(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {}
       ~Multiply() override = default;
+      std::string to_string() const override {
+          return expr_left()->to_string() + " * " + expr_right()->to_string();
+      }
   };
 
   class Divide : public Operator<2> {
     public:
-      Divide(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {
-      }
+      Divide(Expr *expr1, Expr *expr2) : Operator(expr1, expr2) {}
       ~Divide() override = default;
+      std::string to_string() const override {
+          return expr_left()->to_string() + " / " + expr_right()->to_string();
+      }
   };
 
 }  // namespace expression
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "expression.h"
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  namespace e = expression;
+  auto a = e::Constant(1);
+  auto b = e::Variable<int>("x");
+  auto expr = e::Add(&a, &b);
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
